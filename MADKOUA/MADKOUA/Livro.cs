64a855746b509d1390a2c5a567e6b14b8b215e2c
jose_ <jose_@DESKTOP-J1G74PJ>
2016-12-21 17:57:08 +0000

    Adicionado class Livro

---
+++ MADKOUA/MADKOUA/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADKOUA
+{
+    class Livro
+    {
+        public Livro() { }
+        public String Titulo { get; set; }
+        public int Edicao { get; set; }
+        public String ISBN { get; set; }
+        public int NLivrosDisp { get; set; }
+
+        public void AdicionaABaseDados(int ID_Autor, int ID_Editora)
+        {
+            ComunicacaoBD.ExecutaUpdateQuery("INSERT INTO Livro(Autor_ID, Editora_ID, Titulo, Edicao, ISBN, NLivrosDisp) VALUES ('" + ID_Autor + "','" + ID_Editora + "','" + Titulo + "','" + Edicao + "','" + ISBN + "','" + NLivrosDisp +"')");
+        }
+
+        private static ComunicaBD ComunicacaoBD = new ComunicaBD("Data Source=RODRIGOVELOSA\\RODRIGOVELOSA;Initial Catalog=MADKOUADB;Integrated Security=True");
+        public static DataTable ListaLivros()
+        {
+            return ComunicacaoBD.ExecutaQuery("SELECT * FROM Livro");
+        }
+
+    }
+}
