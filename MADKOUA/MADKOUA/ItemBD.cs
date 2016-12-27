c00b32a1073dd026226637cde4f12c613929ece6
jose_ <jose_@DESKTOP-J1G74PJ>
2016-12-27 15:07:40 +0000

    Correção ItemBD

--- MADKOUA/MADKOUA/ItemBD.cs
+++ MADKOUA/MADKOUA/ItemBD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,6 @@ namespace MADKOUA
     {
         public void AdicionaBD();
         public void EliminaBD(int id);
-        public void ListaDB();
+        public DataTable ListaDB();
     }
 }
