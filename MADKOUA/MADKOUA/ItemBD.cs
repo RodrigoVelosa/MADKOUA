bdd5392a4ae2691531e879649767b7686041d9d9
jose_ <jose_@DESKTOP-J1G74PJ>
2016-12-27 15:05:07 +0000

    Interface ItemBD

---
+++ MADKOUA/MADKOUA/ItemBD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADKOUA
+{
+    interface ItemBD
+    {
+        public void AdicionaBD();
+        public void EliminaBD(int id);
+        public void ListaDB();
+    }
+}
