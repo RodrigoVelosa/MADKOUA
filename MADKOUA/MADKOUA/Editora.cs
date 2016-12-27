5433c5f1ac4693683f77bf72c258dbdae054d19b
jose_ <jose_@DESKTOP-J1G74PJ>
2016-12-27 15:20:49 +0000

    Aleteração class Editora

--- MADKOUA/MADKOUA/Editora.cs
+++ MADKOUA/MADKOUA/Editora.cs
@@ -7,28 +7,31 @@ using System.Data;
 
 namespace MADKOUA
 {
-    class Editora
+    class Editora : ItemBD
     {
         public Editora() { }
         public String Nome { set; get; }
         public String Morada { set; get; }
 
-        private static ComunicaBD ComunicacaoBD = new ComunicaBD();
+        
 
 
-        public void AdicionaABaseDados()
+        public void AdicionaBD()
         {
             ComunicacaoBD.ExecutaUpdateQuery("INSERT INTO Editora(Nome, Morada) VALUES ('" + Nome + "','" + Morada + "')");
         }
 
+
+        private static ComunicaBD ComunicacaoBD = new ComunicaBD();
         public static void EliminaEditora(int Editora_ID)
         {
             ComunicacaoBD.ExecutaUpdateQuery("DELETE FROM Editora WHERE ID = " + Editora_ID);
         }
-
         public static DataTable ListaEditores()
         {
             return ComunicacaoBD.ExecutaQuery("SELECT * FROM Editora");
         }
+
+        
     }
 }
